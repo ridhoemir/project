0acfb872a77af3a32523446732db3ef2bb9a5e0b
mhmdfaishal <58391132+mhmdfaishal@users.noreply.github.com>
2020-05-10 00:02:21 +0700

    Add files via upload

--- user_login.php
+++ user_login.php
@@ -36,7 +36,7 @@
           <div class="form-group">
             <div class="col-sm-offset-3 col-sm-9">
               <button type="submit" class="btn btn-primary" name="logincustomer"><span class="glyphicon glyphicon-log-in"></span>&nbsp; Masuk</button>
-              Atau <a href="member/member_daftar.php">Mendaftar!</a>
+              Atau <a href="user/user_daftar.php">Mendaftar!</a>
             </div>
           </div>
         </form>
