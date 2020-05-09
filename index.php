f5dc8e0efdf5ffe3707fc90c665d9f7d72848ae8
mhmdfaishal <58391132+mhmdfaishal@users.noreply.github.com>
2020-05-09 08:23:16 +0700

    updated index terbaru

--- index.php
+++ index.php
@@ -49,8 +49,7 @@ include('config.php');
 			</thead>
 			<tbody>
 				<?php
-				//query ke database SELECT tabel mahasiswa urut berdasarkan id yang paling besar
-				$sql = mysqli_query($koneksi, "SELECT * FROM customer ORDER BY IdCustomer DESC") or die(mysqli_error($koneksi));
+				$sql = mysqli_query($koneksi, "SELECT * FROM customer ORDER BY IdCustomer ASC") or die(mysqli_error($koneksi));
 				//jika query diatas menghasilkan nilai > 0 maka menjalankan script di bawah if
 				if(mysqli_num_rows($sql) > 0){
 					//membuat variabel $no untuk menyimpan nomor urut
